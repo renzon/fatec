df206e3b748da8798ca2ec3d5099d818b0df1caf
renzon <renzon@gmail.com>
2013-03-12 20:58:19 -0300

    aula 06 - Templates

--- src/web/tabela.py
+++ src/web/tabela.py
@@ -2,9 +2,16 @@
 from __future__ import absolute_import, unicode_literals
 
 
-
+class Professor(object):
+    def __init__(self,nome="Renzo",idade=30,salario=100):
+        self.nome=nome
+        self.idade=idade
+        self.salario=salario
 
 def index(write_tmpl):
-    write_tmpl("/templates/tabela.html")
+    alunos=["Shen","Lessa","Mengussi","Fabio"]
+    professores=[Professor(),Professor("Massanori",35,200)]
+    values={"alunos":alunos,"professores":professores}
+    write_tmpl("/templates/table.html",values)
 
 
